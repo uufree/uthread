709570927047c2e6affd9983c7aa01d99073c1e6
SmileLion <1252451603@qq.com>
2017-05-09 22:48:05 +0800

    build CMakeLists and Project

---
+++ build/CMakeFiles/HPRThread.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HPRThread.dir/src/linux/Thread.cpp.o"
+  "CMakeFiles/HPRThread.dir/src/linux/ThreadPool.cpp.o"
+  "libHPRThread.pdb"
+  "libHPRThread.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HPRThread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
